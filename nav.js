01d5349b947659b0179c56b522107b0256e86d7d
Faisal Almail <faisalyounis90@gmail.com>
2025-11-03 15:49:58 +0300

    working on nav and responsiveness

--- nav.js
+++ nav.js
@@ -1,5 +1,12 @@
 console.log("nav js working")
 
-function shownav(){
+function openNav(){
     console.log("showing")
+    document.getElementById("nav").style.display = "flex"
+}
+
+
+function closeNav(){
+    console.log("close nav called")
+    document.getElementById("nav").style.display = "none"
 }
